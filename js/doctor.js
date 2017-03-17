e4ff03ead142fddd9117fee5a00d30fa7c328fa9
Melvin Gruschow <melvin.f.gruschow@gmail.com>
2017-03-17 10:32:23 -0700

    it took me 2.5 hours to get this freaking node stuff to work, so whoops, late commit yes i'm serious, as larry or alecia or chad im incredibly bitter

---
+++ js/doctor.js
@@ -0,0 +1,10 @@
+var apiKey = require("./../.env").apiKey;
+
+function Doctor (){
+    
+}
+
+// Doctor.prototype.myKey = function(){
+//     console.log(this.apiKey);
+// }
+exports.Doctor = Doctor
---
+++ tmp/allConcat.js
@@ -0,0 +1,7 @@
+var Doctor = require("./../js/doctor.js").Doctor;
+
+$(document).ready(function(){
+    console.log('world');
+    var medical = new Doctor;
+
+})
